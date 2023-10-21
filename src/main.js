a81de5f42384c405b2c449c0331fa44960f27895
ALBAB404 <imalbab4@gmail.com>
2023-10-21 18:21:05 +0600

    pinia just install

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import './template.js'
 import router from './router'
-
 import App from './App.vue'
 
+
+const pinia = createPinia()
+
+
 const app =  createApp(App)
+app.use(pinia)
 app.use(router);
 app.mount('#app');
