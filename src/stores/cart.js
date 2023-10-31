21a9002b93fa6baa59298f85acc010e42e9bf37c
ALBAB404 <imalbab4@gmail.com>
2023-10-31 12:22:50 +0600

    add to cart done

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -5,6 +5,7 @@ export const useCart = defineStore('cart', {
   // All Variable  Code Is Here.....................................................................................................
   state: () => ({ 
     cartItem: [],
+    cartItemCount: 0,
     loading: false,
   }),
   
@@ -14,10 +15,19 @@ export const useCart = defineStore('cart', {
       let item = product;
       item = {...item, quantity : 1}
       if (this.cartItem.length > 0) {
-        
+        let boolean = this.cartItem.some((i)=> i.id === item.id);
+        if (boolean) {
+          let index = this.cartItem.findIndex((i)=> i.id === item.id);
+          this.cartItem[index]['quantity'] += 1
+          console.log(this.cartItem);
+        }else{
+          this.cartItem.push(item)
+        }
       }else{
-        this.cartItem.push(product)
+        this.cartItem.push(item)
       }
+
+      this.cartItemCount++
     }
 
 
