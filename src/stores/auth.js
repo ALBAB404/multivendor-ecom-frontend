92f293dabaf72fe021e702f38bd168b97edab209
mdruhulalim <mdruhulalim888888@gmail.com>
2024-02-05 12:29:29 +0600

    jsut testing

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -2,10 +2,19 @@ import { defineStore } from 'pinia'
 import axios from 'axios';
 
 export const useAuth = defineStore('auth', {
+    state:() => ({errors:{}}),
     actions: {
       async login(fromData) {
-       const res = await axios.post(import.meta.env.VITE_API_URL + "/api/v1/admin/login", fromData)
-       console.log(res);
+        try {
+          const res = await axios.post(import.meta.env.VITE_API_URL + "/api/v1/user/login", fromData)
+          if (res.status === 200) {
+              console.log(res.data);
+          }
+        } catch (error) {
+          if (error.response.data) {
+            this.errors = error.response.data.errors 
+          }
+        }
       },
     },
   })
--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,19 +1,33 @@
 
 <script setup>
 
-import {reactive} from '@vue/reactivity';
+import {reactive, ref} from 'vue';
+import { Field, Form, ErrorMessage } from 'vee-validate';
+import * as yup from 'yup';
+
+// validation part
+const schema = yup.object({
+  phone: yup.string().required(),
+  password: yup.string().required().min(8),
+});
 
 import {useAuth} from '@/stores/auth';
+import { storeToRefs } from 'pinia';
 
 const auth = useAuth()
+const { errors } = storeToRefs(auth)
+const isToggle = ref(false)
 
-const from =  reactive({
-  phone: '',
-  password: '',
-})
 
-const onSubmit = async () => {
-  await auth.login(from)
+
+const onSubmit = async (value, action) => {
+  console.log(value);
+  console.log(action);
+  // await auth.login(from)
+}
+
+const togglePassword = () => {
+  isToggle.value = !isToggle.value
 }
 
 </script>
@@ -30,35 +44,31 @@ const onSubmit = async () => {
                 <p>Use your credentials to access</p>
               </div>
               <div class="user-form-group" id="axiosForm">
-                <form class="user-form" @submit.prevent="onSubmit">
+                <Form class="user-form" @submit="onSubmit" :validation-schema="schema" v-slot="{errors}">
                   <!--v-if-->
                   <div class="form-group">
-                    <input
+                    <Field
+                      name="phone"
                       type="text"
                       class="form-control"
+                      :class="{'is-invalid' : errors.phone}"
                       placeholder="phone no"
-                      v-model="from.phone"
-                    /><!--v-if-->
+                    />
+                    <span class="text-danger" v-if="errors.phone" >{{ errors.phone }}</span>
+                    <!--v-if-->
                   </div>
-                  <div class="form-group">
-                    <input
-                      type="password"
+                  <div class="form-group" @click.prevent="togglePassword()">
+                    <Field
+                      name="password"
+                      :type="isToggle ? 'password' : 'text'"
                       class="form-control"
+                      :class="{'is-invalid' : errors.password}"
                       placeholder="password"
-                      v-model="from.password"
                     /><span class="view-password"
-                      ><i class="fas text-success fa-eye"></i></span
-                    ><!--v-if-->
-                  </div>
-                  <div class="form-check mb-3">
-                    <input
-                      class="form-check-input"
-                      type="checkbox"
-                      id="check"
-                      value=""
-                    /><label class="form-check-label" for="check"
-                      >Remember Me</label
+                      ><i class="fas text-success fa-eye" :class="{'fa-eye-slash' : isToggle }"></i></span
                     >
+                    <span class="text-danger" v-if="errors.password" >{{ errors.password }}</span>
+                    <!--v-if-->
                   </div>
                   <div class="form-button">
                     <button type="submit">login</button>
@@ -70,7 +80,7 @@ const onSubmit = async () => {
                       >
                     </p>
                   </div>
-                </form>
+                </Form>
               </div>
             </div>
             <div class="user-form-remind">
